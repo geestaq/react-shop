4dd332f30d96a22dddbec6cde4e0f20fa12fb339
geestaq <gee@vp.pl>
2019-08-15 12:19:53 +0200

    Create client app boilerplate

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'animate.css/animate.min.css';
+
+const Root = () => (
+  <App />
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
